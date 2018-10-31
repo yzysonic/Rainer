9120a69bd2026e315beb12cda7772a46e0de5dab
YangTzui <yzysonic@gmail.com>
2018-10-31 10:29:25 +0900

    同期

---
+++ Assets/Editor/TestEditor.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(Test))]
+public class TestEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        if (GUILayout.Button("MakeTexture"))
+        {
+            MakeTexture();
+        }
+    }
+
+    void MakeTexture()
+    {
+        var test = target as Test;
+        var tex = test.tex;
+        var texArray = new Texture2DArray(tex.width, tex.height, 9, tex.format, tex.mipmapCount>0);
+        for (var i = 0; i < texArray.depth; i++)
+            Graphics.CopyTexture(tex, 0, texArray, i);
+        AssetDatabase.CreateAsset(texArray, "Assets/Resources/Textures/texture_array.asset");
+        Debug.Log(AssetDatabase.GetAssetPath(texArray));
+
+    }
+}
+
+public class CreateMaterialExample : MonoBehaviour
+{
+    [MenuItem("Test/Create Material")]
+    static void CreateMaterial()
+    {
+        // Create a simple material asset
+
+        Material material = new Material(Shader.Find("Specular"));
+        AssetDatabase.CreateAsset(material, "Assets/MyMaterial.mat");
+
+        // Print the path of the created asset
+        Debug.Log(AssetDatabase.GetAssetPath(material));
+    }
+}
--- Assets/YangWorkspace/Test.cs
+++ Assets/YangWorkspace/Test.cs
@@ -3,8 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using System.Linq;
 
-class Test : MonoBehaviour
+public class Test : MonoBehaviour
 {
+    public Texture2D tex;
 
+    private void Start()
+    {
+        //var mat = GetComponent<MeshRenderer>().material;
+        //var paintTexture = Enumerable.Repeat(new RenderTexture(tex.width, tex.height, 0, RenderTextureFormat.Default, RenderTextureReadWrite.Default), 9).ToList();
+        //paintTexture.ForEach(pt => Graphics.Blit(tex, pt));
+        //mat.texture
+        //var renderTex = RenderTexture.GetTemporary(128, 128,);
+        renderTex.dimension = UnityEngine.Rendering.TextureDimension.Tex2DArray;
+        renderTex.volumeDepth = 9;
+        //Graphics.SetRenderTarget(renderTex, 0, CubemapFace.Unknown, )
+    }
 }
