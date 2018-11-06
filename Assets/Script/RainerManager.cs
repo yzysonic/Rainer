1a1ebe3451113af58b65b500d6265a7daefd6248
Umeda <bbrnobby@gmail.com>
2018-11-06 15:30:26 +0900

    UmedaScene追加、RainerManager変更

--- Assets/Script/RainerManager.cs
+++ Assets/Script/RainerManager.cs
@@ -14,24 +14,29 @@ public class RainerManager : MonoBehaviour {
     public float unity_range = 200.0f;
     [Range(1.0f, 10.0f)]
     public float avoid_range = 3.0f;
-    
+
+    [Range(0, 1000)]
+    public int pop_interval = 100;
+
     List<GameObject> rainers = new List<GameObject>();
+
+    public Transform spawnGroup;
+    List<Transform> spawnList = new List<Transform>();
+    int timer;
 
     // Use this for initialization
     void Start()
     {
-        SpawnRainer(new Vector3(10,1,10));
-        SpawnRainer(new Vector3(0,1,10));
-        SpawnRainer(new Vector3(10,1,0));
-        SpawnRainer(new Vector3(10,1,-10));
-        SpawnRainer(new Vector3(0,1,-10));
-        SpawnRainer(new Vector3(-10,1,10));
-        SpawnRainer(new Vector3(-10,1,0));
-        SpawnRainer(new Vector3(-10,1,-10));
+        timer = 0;
+        for (int i = 0; i < spawnGroup.childCount; i++)
+        {
+            spawnList.Add(spawnGroup.GetChild(i));
+            spawnList[i].gameObject.SetActive(false);
+        }
     }
 
     // Update is called once per frame
-    void FixedUpdate()
+    void Update()
     {
         foreach (var rainer in rainers)
         {
@@ -69,6 +74,21 @@ public class RainerManager : MonoBehaviour {
 
                 // 移動する
                 rainer.GetComponent<CharacterController>().SimpleMove(rainerController.move);
+            }
+        }
+
+        timer++;
+        if (timer > pop_interval)
+        {
+            timer = 0;
+
+            if (spawnList.Count > 0)
+            {
+                int index = Random.Range(0, spawnList.Count - 1);
+
+                SpawnRainer(spawnList[index].position);
+
+                spawnList.Remove(spawnList[index]);
             }
         }
     }
