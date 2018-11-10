a317a857204b8df3dc132b9bc18f43dc2b510b70
ZWY <syubunen@gmail.com>
2018-11-10 09:33:43 +0900

    メダル更新

--- Assets/Script/Result/Medal.cs
+++ Assets/Script/Result/Medal.cs
@@ -5,23 +5,42 @@ using UnityEngine.UI;
 
 public class Medal : MonoBehaviour {
 
-    public float medalScl;
-    public AnimationCurve curve;
-    private bool graphEnd1, graphEnd2, graphEnd3, graphEnd4;
+    public float medalRot;
+    public float testRot;
+    public bool checkRot;
+    //private bool graphEnd1, graphEnd2, graphEnd3, graphEnd4;
 
 
     // Use this for initialization
     void Start () {
 
+        medalRot = transform.rotation.y;
+        checkRot = true;
 
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
 
-        //medalScl = curve.Evaluate(Time.time / 2f);
-        //transform.localScale = new Vector3(medalScl, medalScl, medalScl);
-        transform.Rotate(0,45f * Time.deltaTime, 0,Space.World);
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+        if (transform.rotation.y >= 0.7f)
+        {
+            checkRot = false;
+        }
+        else if(transform.rotation.y <= -0.7f)
+        {
+            checkRot = true;
+        }
+
+        if (checkRot)
+        {
+            medalRot = 45f * Time.deltaTime;
+        }
+        else
+        {
+            medalRot = -45f * Time.deltaTime;
+        }
+        testRot = transform.rotation.y;
+        transform.Rotate(0, medalRot, 0,Space.World);
 	}
 }
